83d36cd18fa68b8f7e03b81de47758fbd384b72c
Andreea Iacob <Andreea Iacob@DESKTOP-ECODA5R>
2020-07-14 08:36:24 +0300

    entities created

---
+++ MusicWebApp/Entities/Artist.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MusicWebApp.Entities
+{
+    public class Artist
+    {
+        [Key]
+        public Guid Id { get; set; }
+
+        [Required]
+        [MaxLength(150)]
+        public string FirstName { get; set; }
+
+        [Required]
+        [MaxLength(150)]
+        public string LastName { get; set; }
+
+        public bool? Deleted { get; set; }
+    }
+}
---
+++ MusicWebApp/Entities/Song.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Net;
+using System.Threading.Tasks;
+
+namespace MusicWebApp.Entities
+{
+    public class Song
+    {
+        [Key]
+        public Guid Id { get; set; }
+
+        [Required]
+        [MaxLength(150)]
+        public string Title { get; set; }
+
+        [Required]
+        [MaxLength(150)]
+        public string Genre { get; set;}
+
+        [MaxLength(2500)]
+        public string SongDesciption { get; set; }
+
+        [Required]
+        public Guid ArtistId { get; set; }
+
+        [ForeignKey("ArtistId")]
+        public virtual Artist Artist { get; set; }
+
+
+        public bool? Deleted { get; set; }
+
+    }
+}
